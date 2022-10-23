48d8f97cfc16dd1a5038b8cb50c0f64037edd931
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-10-23 23:45:27 +0530

    probably the cleanest version of the template possible BITCH

--- src/main.cpp
+++ src/main.cpp
@@ -23,39 +23,34 @@ class Game{
         };
     
         int DDA(int X0, int Y0, int X1, int Y1){
-            // calculate dx & dy
+            
             int dx = X1 - X0;
             int dy = Y1 - Y0;
      
-            // calculate steps required for generating pixels
+            
             int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
      
-            // calculate increment in x & y for each steps
+            
             float Xinc = dx / (float)steps;
             float Yinc = dy / (float)steps;
      
-            // Put pixel for each step
+            
             float X = X0;
             float Y = Y0;
             for (int i = 0; i <= steps; i++) {
                 if( SDL_PollEvent(&event) && event.type == SDL_QUIT) break;
-                putpixel(round(X), round(Y)); // put pixel at (X,Y)
-                X += Xinc; // increment in x at each step
-                Y += Yinc; // increment in y at each step
-                SDL_Delay(100); // for visualization of line-
+                putpixel(round(X), round(Y)); 
+                X += Xinc; 
+                Y += Yinc; 
+                SDL_Delay(100); 
             }
             return 0;
         };
     void game_loop(){
-        int i = 1;
         set_color(red);
         while (true){
             if( SDL_PollEvent(&event) && event.type == SDL_QUIT) break;
             DDA(100,200,1000,586);
-            //for(int i = 1, i < WINDOW_HEIGHT, i++){
-             //   putpixel(i,i);
-           // }
-           i++;
            break;
         }
     };
